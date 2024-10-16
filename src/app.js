84a61e015c487989e009b665cf719ae284ca62fe
Marjoriecus <101658277+Marjoriecus@users.noreply.github.com>
2024-10-16 19:35:45 +0000

    finalDomain

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".net", ".us", ".net"];
+
+  for (let x of pronoun) {
+    for (let y of adj) {
+      for (let z of noun) {
+        for (let w of domain)
+          document.querySelector("#dom").innerHTML +=
+            "<h3>" + x + y + z + w + "</h3>";
+      }
+    }
+  }
 };
